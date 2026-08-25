fe19061e8a2de4682c62ed2faf1d3e2f3945d772
Leandro Sousa <ordnael.sousass@gmail.com>
2026-08-25 16:04:17 -0300

    Implementando as regras de negócio do payment

--- src/services/payment.service.ts
+++ src/services/payment.service.ts
@@ -1,3 +1,4 @@
+import { ITrasintion, IWebHookPayload } from '../interfaces/payment.interface';
 import { PaymentRepository } from '../repositors/payment.repository'
 
 export class PaymentService {
@@ -14,13 +15,46 @@ export class PaymentService {
         return payments;
     }
 
-    public async FindById(id: number) {
-        const payment = this.paymentRepository.findById(id);
+    public async FindById(webhook: IWebHookPayload) {
+        const payment = await this.paymentRepository.getById(webhook);
 
         if(!payment) {
             throw new Error('No found payment')
         }
 
         return payment;
+    }
+
+    public async crestes(transintion: ITrasintion) {
+        if(!transintion.amount || transintion.amount <= 0) {
+            throw new Error('Invalid payment amount')
+        }
+        const newPayment = await this.paymentRepository.create(transintion)
+
+        if (!newPayment) {
+            throw new Error('Payment create to FALID');
+        }
+
+        return {'message': 'Payment created carried out with sucessfully'};
+    }
+
+    public async updated(webhook: IWebHookPayload) {
+        const update_pay = await this.paymentRepository.update(webhook)
+
+        if(!update_pay) {
+           throw new Error('Paymemt to update not found')
+        }
+
+        return {'message': 'Payment update corried out with sucessfully'}
+    } 
+    
+    public async deleted(webhook: IWebHookPayload) {
+        const delete_pay = await this.paymentRepository.delete(webhook);
+
+        if(!delete_pay) {
+            throw new Error('Payment to delete not found')
+        }
+
+        return {'message': 'Payment to delete carried with sucessfully'}
     }
 }
