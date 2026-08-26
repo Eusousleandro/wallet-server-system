1c69922f7e1827168cfc89464c6a0ef83a8bcc5f
Leandro Sousa <ordnael.sousass@gmail.com>
2026-08-26 14:05:24 -0300

    Implemeentas as rotas do pay

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 
-import router from ''
+import { router } from '../routes/payment.route';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(router):
 
 export { app };
--- src/routes/payment.route.ts
+++ src/routes/payment.route.ts
@@ -6,10 +6,10 @@ const router = express.Router();
 
 const pay = new PaymentController();
 
-router.get('/checkout', pay.getAll());
-router.get('/checkout/:id', pay.getById());
-router.post('/checkout'pay.create());
-router.put('/checkout/:id'pay.update());
-router.delete('/checkout/:id'pay.delete())
+router.get('/checkout', pay.getAll.bind(pay));
+router.get('/checkout/:id', pay.getById.bind(pay));
+router.post('/checkout'pay.create.bind(pay));
+router.put('/checkout/:id'pay.update.bind(pay));
+router.delete('/checkout/:id'pay.delete.bind(pay))
 
 export { router };
